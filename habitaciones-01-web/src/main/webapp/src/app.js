86b813eaa61530eb82d7597305e0647e0df7abab
c.penaloza <c.penaloza@ISIS2603-0119.sis.virtual.uniandes.edu.co>
2017-03-31 08:34:48 -0500

    Se agrego app.js

---
+++ habitaciones-01-web/src/main/webapp/src/app.js
@@ -0,0 +1,14 @@
+(function (ng) {
+    var app = angular.module('mainApp', [
+        // External dependencies
+        'ui.router',
+        // Internal modules dependencies       
+        'viviendasModule',
+        'habitacionesViviendaModule'
+
+    ]);
+    // Resuelve problemas de las promesas
+    app.config(['$qProvider', function ($qProvider) {
+            $qProvider.errorOnUnhandledRejections(false);
+        }]);
+})(window.angular);
