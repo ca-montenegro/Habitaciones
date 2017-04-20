c476e742eb8308df44a91715be4d548f18d72916
Camilo Montenegro <ca.montenegro@uniandes.edu.co>
2017-04-20 00:19:17 -0500

    modificación usuarios con post

--- habitaciones-01-web/src/main/webapp/src/modules/usuario/usuarios.mod.js
+++ habitaciones-01-web/src/main/webapp/src/modules/usuario/usuarios.mod.js
@@ -1,112 +1,142 @@
-(function (ng){
-var mod = ng.module("usuarioModule", ['ui.router']);
-        mod.constant("usuarioContext", "api/usuarios");
-        mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-        var basePath = 'src/modules/usuario/';
-                $urlRouterProvider.otherwise("/usuariosList");
-                $stateProvider.state('usuarios', {
+(function (ng) {
+    var mod = ng.module("usuarioModule", ['ui.router']);
+    mod.constant("usuarioContext", "api/usuarios");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/usuario/';
+            $urlRouterProvider.otherwise("/usuariosList");
+            $stateProvider.state('usuarios', {
                 url: '/usuarios',
-                        abstract: true,
-                        resolve: {
-                        usuarios: ['$http', 'usuarioContext', function ($http, usuarioContext) {
-                        return $http.get(usuarioContext);
+                abstract: true,
+                resolve: {
+                    usuarios: ['$http', 'usuarioContext', function ($http, usuarioContext) {
+                            return $http.get(usuarioContext);
                         }]
-                        },
-                        views: {
-                        'mainView': {
+                },
+                views: {
+                    'mainView': {
                         templateUrl: basePath + 'usuarios.html',
-                                controller: ['$scope', 'usuarios', function ($scope, usuarios) {
+                        controller: ['$scope', 'usuarios', function ($scope, usuarios) {
                                 $scope.usuariosRecords = usuarios.data;
-                                }]
-                        }
-                        }
-                }).state('usuariosList', {
-        url: '/list',
+                            }]
+                    }
+                }
+            }).state('usuariosList', {
+                url: '/list',
                 parent: 'usuarios',
                 views: {
-                'listView': {
-                templateUrl: basePath + 'usuario.list.html'
+                    'listView': {
+                        templateUrl: basePath + 'usuario.list.html'
+                    }
                 }
-                }
-        }).state('usuarioAdminLogin', {
-        url: '/adminLogin',
+            }).state('usuarioAdminLogin', {
+                url: '/adminLogin',
                 parent: 'usuarios',
                 views: {
-                'listView': {
-                templateUrl: basePath + 'modal.html'
+                    'listView': {
+                        templateUrl: basePath + 'modal.html'
+                    }
                 }
-                }
-        }).state('registrarUsuario', {
-        url:'/registrarUsuario',
+            }).state('registrarUsuario', {
+                url: '/registrarUsuario',
                 parent: 'usuarios',
                 views: {
-                'listView': {
-                templateUrl: basePath + 'registrar.html',
-                        controller: ['$scope', 'usuarios', function ($scope, usuarios, $http, usuarioContext) {
-                        $scope.usuariosRecords = usuarios.data;
-                               
-                                $scope.registrar = function(usuarioContext, $http){
+                    'listView': {
+                        templateUrl: basePath + 'registrar.html',
+                        controller: ['$scope', '$http', '$state', 'usuarios', 'usuarioContext',
+                            function ($scope, $http, $state,  usuarios, usuarioContext) {
+                                //$scope.usuariosRecords = usuarios.data;
 
-                               
-                                        $scope.tempUser = {
-                                                numeroID : $scope.numeroID,
-                                                tipoID :$scope.tipoID,
-                                                nombre : $scope.nombre,
-                                                usuario : $scope.usrname,
-                                                contrasenha :$scope.psw2,
-                                                correo :$scope.correo,
-                                                direccion : $scope.direccion,
-                                                telefono : $scope.telefono,
-                                                numeroTarjeta : $scope.numeroTarjeta,
-                                                image : "https://randomuser.me/api/portraits/men/63.jpg"
+                                $scope.tempUser = {
+                                    numeroID: '',
+                                    tipoID: '',
+                                    nombre: '',
+                                    usuario: '',
+                                    contrasenha: '',
+                                    correo: '',
+                                    direccion: '',
+                                    telefono: '',
+                                    numeroTarjeta: '',
+                                    image: "https://randomuser.me/api/portraits/men/63.jpg"
 
-                                        };
-                                        if (true){
-                                tempUser = $scope.tempUser;
+                                };
+                                console.log($scope.tempUser);
+                                $scope.registrar = function () {
+
+                                    tempUser = $scope.tempUser;
+                                    console.log(tempUser);
+                                    if (true) {
                                         return $http.post(usuarioContext, tempUser)
-                                        .then(function () {
-                                        // $http.post es una promesa
-                                        // cuando termine bien, cambie de estado
-                                        //$state.go('reviewsList');
-                                        console.log('check');
-                                        }, responseError);
-                                        console.log(tempUser);
+                                                .then(function () {
+                                                    // $http.post es una promesa
+                                                    // cuando termine bien, cambie de estado
+                                                    $state.go('usuariosList');
+                                                    console.log('check');
+                                                }, responseError);
+
+                                    }
                                 }
+
+                                this.closeAlert = function (index) {
+                                    $scope.alerts.splice(index, 1);
+                                };
+
+                                // Función showMessage: Recibe el mensaje en String y su tipo con el fin de almacenarlo en el array $scope.alerts.
+                                function showMessage(msg, type) {
+                                    var types = ["info", "danger", "warning", "success"];
+                                    if (types.some(function (rc) {
+                                        return type === rc;
+                                    })) {
+                                        $scope.alerts.push({type: type, msg: msg});
+                                    }
                                 }
+
+                                this.showError = function (msg) {
+                                    showMessage(msg, "danger");
+                                };
+
+                                this.showSuccess = function (msg) {
+                                    showMessage(msg, "success");
+                                };
+
+                                var self = this;
+                                function responseError(response) {
+
+                                    self.showError(response.data);
+                                }
+                            }]
+                    }
+                }
+
+
+            }).state('usuarioDetail', {
+                url: '/{usuarioId:int}/detail',
+                parent: 'usuarios',
+                param: {
+                    usuarioId: null
+                },
+                resolve: {
+                    currentUsuario: ['$http', 'usuarioContext', '$stateParams', function ($http, usuarioContext, $params) {
+                            return $http.get(usuarioContext + '/' + $params.usuarioId);
                         }]
-                        }
-                        }
-
-
-                        }).state('usuarioDetail', {
-                        url:'/{usuarioId:int}/detail',
-                                parent: 'usuarios',
-                                param:{
-                                usuarioId: null
-                                },
-                                resolve : {
-                                currentUsuario : ['$http', 'usuarioContext', '$stateParams', function ($http, usuarioContext, $params){
-                                return $http.get(usuarioContext + '/' + $params.usuarioId);
+                },
+                views: {
+                    listView: {
+                        templateUrl: basePath + 'usuario.list.html',
+                        resolve: {
+                            usuarios: ['$http', 'usuarioContext', function ($http, usuarioContext) {
+                                    return $http.get(usuarioContext);
                                 }]
-                                },
-                                views:{
-                                listView:{
-                                templateUrl: basePath + 'usuario.list.html',
-                                        resolve : {
-                                        usuarios :['$http', 'usuarioContext', function ($http, usuarioContext){
-                                        return $http.get(usuarioContext);
-                                        }]
-                                        },
-                                },
-                                        detailView:{
+                        },
+                    },
+                    detailView: {
 
-                                        templateUrl: basePath + 'usuario.detail.html',
-                                                controller: ['$scope', 'currentUsuario', function ($scope, currentUsuario) {
-                                                console.log(currentUsuario.data);
-                                                        $scope.currentUsuario = currentUsuario.data;
-                                                }]
-                                        }
-                                }
-                        });
-                        }]);
-                        })(window.angular);
+                        templateUrl: basePath + 'usuario.detail.html',
+                        controller: ['$scope', 'currentUsuario', function ($scope, currentUsuario) {
+                                console.log(currentUsuario.data);
+                                $scope.currentUsuario = currentUsuario.data;
+                            }]
+                    }
+                }
+            });
+        }]);
+})(window.angular);
