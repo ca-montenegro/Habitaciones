3e1dd64e110835eb53aafe38d0400128c08b445e
l.maya10 <l.maya10@ISIS2603-0121.sis.virtual.uniandes.edu.co>
2017-05-14 21:54:23 -0500

    funciona get general no detail

--- habitaciones-01-web/src/main/webapp/src/modules/habitaciones/habitaciones.mod.js
+++ habitaciones-01-web/src/main/webapp/src/modules/habitaciones/habitaciones.mod.js
@@ -1,33 +1,36 @@
 (function (ng){
-    var mod = ng.module("habitacionModule", ['ui.router']);
-    mod.constant("habitacionesContext", "api/habitaciones");
-    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    var mod = ng.module('habitacionModule', ['ui.router']);
+    mod.constant('habitacionesContext', 'api/habitaciones');
+    mod.config(['$stateProvider', '$urlRouterProvider', 
+        function ($stateProvider, $urlRouterProvider) {
             var basePath = 'src/modules/habitaciones/';
-            $urlRouterProvider.otherwise("/habitacionesList");
+            $urlRouterProvider.otherwise('/habitacionesList');
 
             $stateProvider.state('habitaciones', {
                 url: '/habitaciones',
                 abstract: true,
                 resolve: {
-                    habitaciones: ['$http', function ($http) {
-                            return $http.get('data/habitaciones.json');
-                        }]
+                    habitaciones: ['$http','habitacionesContext', 
+                        function ($http, habitacionesContext) {
+                            return $http.get(habitacionesContext);
+                        },]
                 },
                 views: {
                     'mainView': {
                         templateUrl: basePath + 'habitaciones.html',
-                        controller: ['$scope', 'habitaciones', function ($scope, habitaciones) {
+                        controller: ['$scope', 'habitaciones', 
+                                    function ($scope, habitaciones) {
                                 $scope.habitacionesRecords = habitaciones.data;
-                            }]
-                    }
-                }
+                            },]
+                    },
+                },
             }).state('habitacionesList', {
                 url: '/lista',
                 parent: 'habitaciones',
                 views: {
                     'listView': {
-                        templateUrl: basePath + 'habitaciones.list.html'
-                    }
+                        templateUrl: basePath + 'habitaciones.list.html',
+                    },
                 }
             }).state('habitacionDetail', {
                 url: '/{habitacionId:int}/detail',
@@ -35,16 +38,23 @@
                 param: {
                     habitacionId: null
                 },
+                resolve:{
+                    habitacionActual:
+                            ['$http','habitacionesContext', '$stateParams',
+                        function($http, habitacionesContext, $params){
+                            return $http.get(habitacionesContext + 
+                                    '/'+$params.habitacionId);
+                        },]
+                },
                 views: {
                     'detailView': {
                         templateUrl: basePath + 'habitaciones.detail.html',
-                        controller: ['$scope', '$stateParams', function ($scope, $params) {
-                                $scope.currentHabitacion = $scope.habitacionesRecords[$params.habitacionId-1];
+                        controller: ['$scope', 'habitacionActual', 
+                                    function ($scope,  habitacionActual ) {
+                                $scope.habitacionActual = habitacionActual.data;
                             }]
-                    }
-
+                    },
                 }
-
             });
         }]);
 })(window.angular);
