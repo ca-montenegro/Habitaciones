b96d0c948e85473def395ae66c41141f8cfe495f
Andrés David Laiton Vargas <ad.laiton10@uniandes.edu.co>
2017-05-17 12:24:07 -0500

    Comentarios en el entity de reservas.

--- habitaciones-01-logic/src/main/java/co/edu/uniandes/csw/habitaciones/entities/ReservaEntity.java
+++ habitaciones-01-logic/src/main/java/co/edu/uniandes/csw/habitaciones/entities/ReservaEntity.java
@@ -36,100 +36,125 @@ import uk.co.jemos.podam.common.PodamExclude;
 @Entity
 public class ReservaEntity implements Serializable{
     
+    //Constructor vacio
     public ReservaEntity(){
         
     }
     
     private static final long serialVersionUID = 1L;
     
+    //Codigo de la reserva
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @PodamExclude
     private Long codigoReserva;
     
+    //Fecha de inicio de la reserva
     @Temporal(TemporalType.TIMESTAMP) 
     private Date fechaInicio;
     
+    //Fecha de fin de la reserva
     @Temporal(TemporalType.TIMESTAMP) 
     private Date fechaFin;
     
+    //Costo de la reserva
     private double costo;
     
+    //Estado de la reserva
     @PodamExclude
     private char estado;
     
+    //Multa de la reserva
     @OneToOne (fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @PodamExclude
     private MultaEntity multa;
     
+    //Habitacion ede la reserva
     @ManyToOne(cascade = CascadeType.PERSIST)
     @PodamExclude
     private HabitacionEntity habitacion;
     
+    //Vivienda de la reserva
     @ManyToOne
     @PodamExclude
     private ViviendaEntity vivienda;
 
+    //Get de habtiacion
     public HabitacionEntity getHabitacion() {
         return habitacion;
     }
 
+    //Definir la habitación
     public void setHabitacion(HabitacionEntity habitacion) {
         this.habitacion = habitacion;
     }
 
+    //Get de la vivienda
     public ViviendaEntity getVivienda() {
         return vivienda;
     }
-
+    
+    //Definir la vivienda
     public void setVivienda(ViviendaEntity vivienda) {
         this.vivienda = vivienda;
     } 
 
+    //Get de la reserva
     public Long getCodigoReserva() {
         return codigoReserva;
     }
 
+    //Definir el codigo de la reserva
     public void setCodigoReserva(Long codigoReserva) {
         this.codigoReserva = codigoReserva;
     }
 
+    //Get de la fecha de inicio
     public Date getFechaInicio() {
         return fechaInicio;
     }
 
+    //Definir la fecha de inicio
     public void setFechaInicio(Date fechaInicio) {
         this.fechaInicio = fechaInicio;
     }
 
+    //Get de la fecha de fin
     public Date getFechaFin() {
         return fechaFin;
     }
 
+    //Definir la fecha de fin
     public void setFechaFin(Date fechaFin) {
         this.fechaFin = fechaFin;
     }
 
+    //Get del costo de la reserva
     public double getCosto() {
         return costo;
     }
 
+    //Definir el costo de la reserva
     public void setCosto(double costo) {
         this.costo = costo;
     }
 
+    //Get del estado de la reserva
     public char getEstado() {
         return estado;
     }
 
+    //Definir el estado de la reserva
     public void setEstado(char estado) {
         this.estado = estado;
     }
     
+    //Get de la multa de la reserva
      public MultaEntity getMulta() {
         return multa;
     }
 
+    //Definir la multa de la reserva
     public void setMulta(MultaEntity multa) {
         this.multa = multa;
     }
